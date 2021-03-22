a9d6ee8aa36a3c6ecc20d57e4485747a070dfd31
CarlosGRJ <carlosgrj@outlook.com>
2021-03-22 15:55:40 -0600

    Building Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
+import { ImageLinkForm } from './components/ImageLinkForm/ImageLinkForm';
+import { Logo } from './components/Logo/Logo';
+import { Navigation } from './components/Navigation/Navigation';
+import { Rank } from './components/Rank/Rank';
+
+const particlesOptions = {
+  particles: {
+    // shape: {
+    //   type: 'images',
+    //   image: [
+    //     { src: 'path/to/first/image.svg', height: 20, width: 20 },
+    //     { src: 'path/to/second/image.jpg', height: 20, width: 20 },
+    //   ],
+    // },
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Particles params={particlesOptions} className='particles' />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*
+      <FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Carlos, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
